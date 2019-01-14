2f3bf7d2eba03ef9d4f7b3808db98ee1d86f9c17
nader2929 <nadersobhi1@hotmail.com>
2019-01-14 21:23:17 +0000

    Kinda works

--- test.py
+++ test.py
@@ -1,20 +1,26 @@
 def dcp18():
-    nums = [10, 5, 2, 7, 8, 7]    
+    nums = [10, 5, 2, 7, 8, 7]
     k = input("Give us a number: ")
     type(k)
     for x in range (len(nums)):
         maximum = 0
-        if (x + int(k) - 1 < len(nums)):
+        c = x + int(k)-1
+        if c < len(nums):
             maximum = nums[x]
             comp = 0
-            if (nums[x+2] < nums[x+1]):
+            if nums[x+2] < nums[x+1]:
                 comp = nums[x+1]
             else:
                 comp = nums[x+2]
-            if (comp < maximum):
+            if comp < maximum:
                 print(maximum)
             else:
                 print(comp)
     print ("Done with DCP18")
-                
-dcp18()
+while True:
+    choice = input("Which DCP do you wanna run?")
+    type(choice)
+    if choice=="18":
+        dcp18()
+    else:
+        print("Fuck Off")
